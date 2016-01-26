645caac60a4ef5efc868d43bb88c000ba76ecbcb
wondollaballa <onedough83@gmail.com>
2016-01-26 15:00:51 -0800

    setup update

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Input;
+use Validator;
+use Redirect;
+use Hash;
+use Request;
+use Route;
+use Response;
+use Auth;
+use URL;
+use Session;
+use Laracasts\Flash\Flash;
+use View;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    //
+}
---
+++ app/Page.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+use App\Job;
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+
+}
