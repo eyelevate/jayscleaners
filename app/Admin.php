740387e7f8d62263fae9f8eebaf99a0eeec6a246
wondollaballa <onedough83@gmail.com>
2016-05-29 18:20:37 -0700

    update serverat

--- app/Admin.php
+++ app/Admin.php
@@ -58,27 +58,29 @@ class Admin extends Model
     	$last_created_at = $company->created_at;
     	$update = [];
     	$update_rows = 0;
+        $check_date = date('Y-m-d H:i:s',$sever_at);
 
     	if (is_numeric(strtotime($server_at))) {
+            $check_date = date('Y-m-d H:i:s',$sever_at);
     		// get all data from models
-    		$colors = Color::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$companies = Company::where('updated_at','>=',$server_at)->where('id',$company_id)->get();
-    		$custids = Custid::where('updated_at','>=',$server_at)->get();
-    		// $customers = Customer::where('updated_at',' >= ',$server_at)->get();
-    		$deliveries = Delivery::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$discounts = Discount::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$inventories = Inventory::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$inventory_items = InventoryItem::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$invoices = Invoice::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$invoice_items = InvoiceItem::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$memos = Memo::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$printers = Printer::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$rewards = Reward::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$reward_transactions = RewardTransaction::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$schedules = Schedule::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$taxes = Tax::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$transactions = Transaction::where('updated_at','>=',$server_at)->where('company_id',$company_id)->get();
-    		$users = User::where('updated_at','>=',$server_at)->get();
+    		$colors = Color::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$companies = Company::where('updated_at','>=',$check_date)->where('id',$company_id)->get();
+    		$custids = Custid::where('updated_at','>=',$check_date)->get();
+    		// $customers = Customer::where('updated_at',' >= ',$check_date)->get();
+    		$deliveries = Delivery::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$discounts = Discount::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$inventories = Inventory::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$inventory_items = InventoryItem::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$invoices = Invoice::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$invoice_items = InvoiceItem::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$memos = Memo::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$printers = Printer::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$rewards = Reward::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$reward_transactions = RewardTransaction::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$schedules = Schedule::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$taxes = Tax::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$transactions = Transaction::where('updated_at','>=',$check_date)->where('company_id',$company_id)->get();
+    		$users = User::where('updated_at','>=',$check_date)->get();
     		// 
     		if(count($colors) > 0){
     			$update['colors'] = $colors;
