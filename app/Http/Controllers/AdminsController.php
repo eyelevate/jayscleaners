c18e8fa8d421d7c9875f6155987f147f7e1a0754
wondollaballa <onedough83@gmail.com>
2016-06-26 11:18:00 -0700

    update

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -343,9 +343,7 @@ class AdminsController extends Controller
         if($server_at){
             $server_at = str_replace('_',' ',$server_at);
             $up =json_decode(str_replace('up=', '', $up),true);
-            $up = str_replace('__', ' ', $up);
             $upd = json_decode(str_replace('upd=','',$upd),true);
-            $upd = str_replace('__', ' ', $upd);
             $authenticate = Company::where('id',$id)->where('api_token',$api_token)->first();
 
 
