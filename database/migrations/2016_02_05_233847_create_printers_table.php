d1be8d66fa3cc7068ff5d15a947fd08cc5f77617
wondollaballa <onedough83@gmail.com>
2016-07-15 13:53:44 -0700

    migration

--- database/migrations/2016_02_05_233847_create_printers_table.php
+++ database/migrations/2016_02_05_233847_create_printers_table.php
@@ -18,6 +18,8 @@ class CreatePrintersTable extends Migration
             $table->string('name',100)->nullable();
             $table->string('model',100)->nullable();
             $table->string('nick_name',100)->nullable();
+            $table->string('vendor_id',20)->nullable();
+            $table->string('product_id',20)->nullable();
             $table->tinyInteger('type', false, true)->nullable();
             $table->tinyInteger('status', false, true)->nullable();
             $table->softDeletes();
