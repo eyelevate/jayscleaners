f6bf145832d16669c7fa8aed7e51ddcabc156a2b
wondollaballa <onedough83@gmail.com>
2016-09-19 07:15:47 -0700

    update

--- resources/views/partials/layouts/navigation_logged_in.blade.php
+++ resources/views/partials/layouts/navigation_logged_in.blade.php
@@ -3,14 +3,14 @@
 	<ul>
 		<li><a href="{{ route('pages_index') }}">Home</a></li>
 		<li class="submenu">
-			<a href="#"><small>Hello </small><strong>{{ $auth->username }}</strong></a>
+			<a href="#"><small>Hello </small><strong>{{ Auth::user()->username }}</strong></a>
 			<ul>
 				<li><a href="{{ route('delivery_index') }}">Your Deliveries</a></li>
 				<li><a href="left-sidebar.html">Services</a></li>
 				<li><a href="right-sidebar.html">Business Hours</a></li>
 				<li><a href="contact.html">Contact Us</a></li>
 				<li class="submenu">
-					<a href="#">{{ $auth->username }} menu</a>
+					<a href="#">{{ Auth::user()->username }} menu</a>
 					<ul>
 						<li><a href="#">Dolore Sed</a></li>
 						<li><a href="#">Consequat</a></li>
