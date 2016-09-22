fedb5b48f8b5f122ca2855484bd780f857cbbafa
wondollaballa <onedough83@gmail.com>
2016-09-22 11:15:03 -0700

    update

--- resources/views/emails/status_update.blade.php
+++ resources/views/emails/status_update.blade.php
@@ -220,6 +220,54 @@
       </tbody>
    </table>
 </div>
+<div class="block">
+   <!-- start textbox-with-title -->
+   <table width="100%" bgcolor="#f6f4f5" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" st-sortable="fulltext">
+         <tbody>
+         <tr>
+               <td>
+                     <table bgcolor="#ffffff" width="580" cellpadding="0" cellspacing="0" border="0" align="center" class="devicewidth" modulebg="edit">
+                        <tbody>
+                     <!-- Spacing -->
+                     <tr>
+                        <td width="100%" height="10"></td>
+                     </tr>
+                     <!-- Spacing -->
+                     <!-- Spacing -->
+                     <tr>
+                        <td width="100%" height="10"></td>
+                     </tr>
+                     <!-- Spacing -->
+                     <!-- button -->
+                     <tr>
+                        <td>
+                           <table height="36" align="center" valign="middle" border="0" cellpadding="0" cellspacing="0" class="tablet-button" st-button="edit">
+                             <tbody>
+                                <tr>
+                                   <td width="auto" align="center" valign="middle" height="36" style=" background-color:#0db9ea; border-top-left-radius:4px; border-bottom-left-radius:4px;border-top-right-radius:4px; border-bottom-right-radius:4px; background-clip: padding-box;font-size:13px; font-family:Helvetica, arial, sans-serif; text-align:center;  color:#ffffff; font-weight: 300; padding-left:25px; padding-right:25px;">                                                
+                                    <span style="color: #ffffff; font-weight: 300;">
+                                       <?php echo $button; ?>
+                                       </span>
+                                   </td>
+                                </tr>
+                             </tbody>
+                           </table>                                       
+                        </td>
+                     </tr>
+                     <!-- /button -->
+                     <!-- Spacing -->
+                     <tr>
+                        <td width="100%" height="30"></td>
+                     </tr>
+                           <!-- Spacing -->
+                        </tbody>
+               </table>
+               </td>
+         </tr>
+      </tbody>
+   </table>
+      <!-- end of textbox-with-title -->
+</div>
 @if ($body_2)
 <div class="block">
    <!-- start textbox-with-title -->
@@ -282,54 +330,7 @@
 </div>
 
 @endif
-<div class="block">
-   <!-- start textbox-with-title -->
-   <table width="100%" bgcolor="#f6f4f5" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" st-sortable="fulltext">
-      	<tbody>
-			<tr>
-            	<td>
-               		<table bgcolor="#ffffff" width="580" cellpadding="0" cellspacing="0" border="0" align="center" class="devicewidth" modulebg="edit">
-                  		<tbody>
-							<!-- Spacing -->
-							<tr>
-								<td width="100%" height="10"></td>
-							</tr>
-							<!-- Spacing -->
-							<!-- Spacing -->
-							<tr>
-								<td width="100%" height="10"></td>
-							</tr>
-							<!-- Spacing -->
-							<!-- button -->
-							<tr>
-								<td>
-									<table height="36" align="center" valign="middle" border="0" cellpadding="0" cellspacing="0" class="tablet-button" st-button="edit">
-									  <tbody>
-									     <tr>
-									        <td width="auto" align="center" valign="middle" height="36" style=" background-color:#0db9ea; border-top-left-radius:4px; border-bottom-left-radius:4px;border-top-right-radius:4px; border-bottom-right-radius:4px; background-clip: padding-box;font-size:13px; font-family:Helvetica, arial, sans-serif; text-align:center;  color:#ffffff; font-weight: 300; padding-left:25px; padding-right:25px;">                                                
-									        	<span style="color: #ffffff; font-weight: 300;">
-									        		<?php echo $button; ?>
-									           	</span>
-									        </td>
-									     </tr>
-									  </tbody>
-									</table>                                       
-								</td>
-							</tr>
-							<!-- /button -->
-							<!-- Spacing -->
-							<tr>
-								<td width="100%" height="30"></td>
-							</tr>
-                     		<!-- Spacing -->
-                  		</tbody>
-					</table>
-            	</td>
-     		</tr>
-		</tbody>
-	</table>
-   	<!-- end of textbox-with-title -->
-</div>
+
 <div class="block">
    <!-- Start of preheader -->
    <table width="100%" bgcolor="#f6f4f5" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" st-sortable="postfooter">
