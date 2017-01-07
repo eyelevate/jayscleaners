6a4e893878542df69b2f8eea51cf0401a770ecbc
wondollaballa <onedough83@gmail.com>
2017-01-06 18:08:57 -0800

    update

--- app/Schedule.php
+++ app/Schedule.php
@@ -361,7 +361,6 @@ class Schedule extends Model
         $company_id = null;
         if (count($schedules) > 0) {
             foreach ($schedules as $schedule) {
-                Job::dump($schedule->id);
                 $company_id = $schedule->company_id;
                 $address_id = $schedule->pickup_address;
                 $addresses = Address::find($address_id);
