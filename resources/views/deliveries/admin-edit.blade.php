0dc6e841ab302c2b144fe6949868d018a875e2ed
wondollaballa <onedough83@gmail.com>
2017-02-09 07:54:07 -0800

    update

--- resources/views/deliveries/admin-edit.blade.php
+++ resources/views/deliveries/admin-edit.blade.php
@@ -119,7 +119,7 @@
                                 @if ($schedule->pickup_delivery_id > 0)
                                 {{ Form::select('pickup_time',$time_options,$selected_delivery_id,['id'=>'pickuptime','class'=>'form-control']) }}
                                 @else
-                                {{ Form::select('pickup_time',[''=>'select time'],null,['id'=>'pickuptime','class'=>'form-control', 'disabled'=>"true"]) }}
+                                {{ Form::select('pickup_time',[''=>'select time'],null,['id'=>'pickuptime','class'=>'form-control']) }}
                                 @endif
                                 
                                 @if ($errors->has('pickup_time'))
