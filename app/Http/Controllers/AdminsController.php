6aec7fc717d8dbe50733d573b5fac7c977843de9
wondollaballa <onedough83@gmail.com>
2017-03-13 16:52:04 -0700

    array

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -1300,7 +1300,7 @@ class AdminsController extends Controller
     public static function postApiInvoiceData(Request $request) {
         $invoice_id = $request->id;
         $invoice_items = InvoiceItem::where('invoice_id',$invoice_id)->get();
-        return response()->json([$invoice_items]);
+        return response()->json($invoice_items);
     }
 
     
