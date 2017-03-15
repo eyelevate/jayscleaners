afd65786f7de4334ace2750b8e22bb5d08debefc
wondollaballa <onedough83@gmail.com>
2017-03-14 17:37:09 -0700

    update

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -1301,7 +1301,7 @@ class AdminsController extends Controller
     public static function getApiInvoiceData(Request $request) {
         $invoice_id = '076076';
         $invoice_items = InvoiceItem::prepareEdit(InvoiceItem::where('invoice_id',$invoice_id)->get());
-        $tags = Tags::prepareTags($invoice_items);
+        $tags = Tag::prepareTags($invoice_items);
 
         return response()->json($tags);
     }
@@ -1309,7 +1309,7 @@ class AdminsController extends Controller
     public static function postApiInvoiceData(Request $request) {
         $invoice_id = $request->id;
         $invoice_items = InvoiceItem::prepareEdit(InvoiceItem::where('invoice_id',$invoice_id)->get());
-        $tags = Tags::prepareTags($invoice_items);
+        $tags = Tag::prepareTags($invoice_items);
 
         return response()->json($tags);
     }
