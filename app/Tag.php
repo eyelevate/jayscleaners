6dabb1b0e8540fc6acab9f44ad29a346c1b4f1de
wondollaballa <onedough83@gmail.com>
2017-03-14 17:17:07 -0700

    update

--- app/Tag.php
+++ app/Tag.php
@@ -9,7 +9,7 @@ class Tag extends Model
     use SoftDeletes;
 
     public static function prepareTag($data, $invoice_id) {
-    	$tag_update = ['invoice'=>[],'items'=>[]];
+    	$tag_update = [0=>[],1=>[]];
     	$tags = Tag::where('invoice_id',$invoice_id)
     		->where('status',1)
     		->get();
