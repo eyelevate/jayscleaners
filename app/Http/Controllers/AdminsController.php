a8ddc216ef733ee6f9dee1b8c1eb918b788b5731
wondollaballa <onedough83@gmail.com>
2017-03-29 17:42:54 -0700

    update search

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -1560,6 +1560,61 @@ class AdminsController extends Controller
         return response()->json("false");
     }
 
+    public function postSingleUserData(Request $request) {
+        $search = $request->search;
+        $data = [];
+
+        // check if numeric
+        if (is_numeric($search)) {
+            // check length if 7-10 digits its a phone number
+            if (strlen($search) > 6) {
+                $data = User::where('phone','like',"%".$search."%")->get();
+
+            } elseif(strlen($search) == 6) { // if 6 digits its an invoice
+                $invoices = Invoice::find($search);
+                if ($invoices) {
+                    $user_id = $invoices->customer_id;
+                    $data = User::where('id',$user_id)->get();
+                }
+            } else { // if less than 6 its an id
+                $data = User::where('id',$search)->get();
+            }
+            
+
+            
+        } else { // probably a name turn it into an array then check for last and first name
+            // check marks
+            $custids = CustId::where('mark',$search)->get();
+            if (count($custids) > 0) {
+                foreach ($custids as $custid) {
+                    $user_id = $custid->customer_id;
+                    $data = User::where('id',$user_id)->get();
+                }
+            } else {
+                // convert string into an array by words
+                $full_name = explode(' ', $search);
+                
+                if (count($full_name) > 1) {  // check full name
+                    $last_name = $full_name[0];
+                    $first_name = $full_name[1];
+                    $data = User::where('last_name','like',"%".$last_name."%")
+                        ->where('first_name','like',"%".$first_name."%")
+                        ->orderBy('last_name','asc')
+                        ->get();
+
+                } else { // check last name
+                    $data = User::where('last_name','like',"%".$last_name."%")
+                        ->get();
+                }
+
+               
+            }
+            
+        }
+
+        return response()->json($data);
+    }
+
     
 
 
