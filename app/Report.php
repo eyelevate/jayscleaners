251baf9949be10e51798807027629754c16f5f05
wondollaballa <onedough83@gmail.com>
2017-04-18 21:22:35 -0700

    update

--- app/Report.php
+++ app/Report.php
@@ -291,7 +291,7 @@ class Report extends Model
             foreach ($completed_invoice_ids as $iidkey => $iidvalue) {
                 $check_inventory_id = InvoiceItem::where('invoice_id',$iidvalue)->limit(1)->pluck('inventory_id');
                 Job::dump($check_inventory_id);
-                array_push($itemsToInvoice[$check_inventory_id], $iidvalue);
+ 
                 
 
             }
