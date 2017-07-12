84339b23156679eaefc63b6d175f893c16e42d11
wondollaballa <onedough83@gmail.com>
2017-07-12 11:59:50 -0700

    push update

--- app/Report.php
+++ app/Report.php
@@ -164,8 +164,8 @@ class Report extends Model
         $ed = date('Y-m-d 11:16:00',$end);
         $total_view = Transaction::whereBetween('created_at',[$sd,$ed])->where('company_id',$company_id)->where('type','<',5)->get();
 
-        foreach ($total_view as $ttv) {
-            Job::dump($ttv->id.' -- '.$ttv->customer_id.' -- '.$ttv->created_at);
+        foreach ($total_view as $kk => $ttv) {
+            Job::dump($kk.' -- '.$ttv->id.' -- '.$ttv->customer_id.' -- '.$ttv->created_at);
         }
 
         $cash_pretax = Transaction::whereBetween('created_at',[$start_date,$end_date])->where('company_id',$company_id)->where('type',3)->sum('pretax');
