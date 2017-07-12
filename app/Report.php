c52940bfa001356f5608a7c37b8f95d9bf48d9c7
wondollaballa <onedough83@gmail.com>
2017-07-12 11:29:34 -0700

    update

--- app/Report.php
+++ app/Report.php
@@ -156,6 +156,11 @@ class Report extends Model
         $discount = Transaction::whereBetween('created_at',[$start_date,$end_date])->where('company_id',$company_id)->where('type','<',5)->sum('discount');
         $credit = Transaction::whereBetween('created_at',[$start_date,$end_date])->where('company_id',$company_id)->where('type','<',5)->sum('credit');
         $total = Transaction::whereBetween('created_at',[$start_date,$end_date])->where('company_id',$company_id)->where('type','<',5)->sum('total');
+        dd($pretax);
+        dd($tax);
+        dd($discount);
+        dd($credit);
+        dd($total);
 
         $cash_pretax = Transaction::whereBetween('created_at',[$start_date,$end_date])->where('company_id',$company_id)->where('type',3)->sum('pretax');
         $cash_tax = Transaction::whereBetween('created_at',[$start_date,$end_date])->where('company_id',$company_id)->where('type',3)->sum('tax');
