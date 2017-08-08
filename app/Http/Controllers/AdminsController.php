c11781a1dd4aa94a896db7a5f4a610c61ab0811a
wondollaballa <onedough83@gmail.com>
2017-08-08 13:51:52 -0700

    update admin

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -1728,6 +1728,42 @@ class AdminsController extends Controller
         return response()->json($invoices);
     }
 
+    public function getApiSearchCustomer($query = null) {
+        $query_word_count = str_word_count($query);
+        $results = [];
+        if ($query_word_count > 0) {
+            //check if string
+            if (is_string($query)) {
+                $string_array = explode(" ", $query);
+                $last_name = $string_array[0];
+                $first_name = $string_array[1];
+                // look by last_name and first name
+                $results = User::where('last_name','like',"%".$last_name."%")
+                ->where('first_name','like','%'.$first_name.'%')
+                ->get();
+            }  
+        } else {
+            if (is_numeric($query)) {
+                if (strlen($query) > 5) { // Phone
+                    $results = User::where('phone',$query)->get();
+                } else {
+                    $results = User::where('id',$query)->get();
+                }
+            } else { // check marks table
+                $marks = Custid::where('mark',$query)->get();
+                if (count($marks) > 0) {
+                    foreach ($marks as $mark) {
+                        $customer_id = $mark->customer_id;
+                        $results = User::where('id',$customer_id)->get();
+                        break;
+                    }
+                }
+            }
+        }
+
+        return response()->json($results);
+    }
+
 
     public function postApiSearchCustomer(Request $request) {
         $query = $request->query;
