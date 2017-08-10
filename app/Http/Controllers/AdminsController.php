e7580da6f49cd5ece7fc62abb436649fb04a00b9
wondollaballa <onedough83@gmail.com>
2017-08-09 17:57:58 -0700

    update

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -2022,13 +2022,18 @@ class AdminsController extends Controller
 
     public function postApiEditInvoice(Request $request) {
         $invoice = Invoice::find($request->invoice_id);
+        $qty = InvoiceItem::where('invoice_id',$request->invoice_id)->sum('quantity');
+        $pretax = InvoiceItem::where('invoice_id',$request->invoice_id)->sum('pretax');
+        $tax = InvoiceItem::where('invoice_id',$request->invoice_id)->sum('tax');
+        $total = InvoiceItem::where('invoice_id',$request->invoice_id)->sum('total');
+
         $i = json_decode($request->invoice,true);
         $invoice->company_id =$i['company_id'];
         $invoice->customer_id =$i['customer_id'];
-        $invoice->quantity = $i['quantity'];
-        $invoice->pretax = $i['pretax'];
-        $invoice->tax = $i['tax'];
-        $invoice->total = $i['total'];
+        $invoice->quantity = $quantity;
+        $invoice->pretax = $pretax;
+        $invoice->tax = $tax;
+        $invoice->total = $total;
         $invoice->due_date = $i['due_date'];
         $invoice->reward_id = ($i['reward_id']) ? $i['reward_id'] : NULL;
         $invoice->discount_id = ($i['discount_id'] != '') ? $i['discount_id'] : NULL;
