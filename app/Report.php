d85e3d120728d1d8899c6c5ed8e84ece5f953cd0
wondollaballa <onedough83@gmail.com>
2017-09-21 15:46:43 -0700

    update

--- app/Report.php
+++ app/Report.php
@@ -275,7 +275,10 @@ class Report extends Model
         $inventories = Inventory::where('company_id',$company_id)->get();
         if(count($inventories) > 0) {
             foreach ($inventories as $inventory) {
-                dd($inventory->invoiceItems()->where('invoice_id',$completed_invoice_ids)->select(\DB::raw('SUM(quantity) as quantity'),\DB::raw('SUM(pretax) as pretax'))->first());
+                if($inventory->invoiceItems) {
+                    dd($inventory->invoiceItems()->where('invoice_id',$completed_invoice_ids)->select(\DB::raw('SUM(quantity) as quantity'),\DB::raw('SUM(pretax) as pretax'))->first());
+                }
+                
             }
         }
         dd($inventories);
