69915258742794a36748aba3de1fa98c94dc9ff9
wondollaballa <onedough83@gmail.com>
2018-10-14 00:09:38 -0500

    test api

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -2944,11 +2944,12 @@ class AdminsController extends Controller
                 ->orderBy('id','desc')->get();
 
             $invoices->chunk(100,function($inv) {
-                $inv->each(function($v, $k) {
-                    $inv_items = $v->invoice_items;
-                    $v['invoice_items'] = $inv_items;
+                var_dump($inv);
+                // $inv->each(function($v, $k) {
+                //     $inv_items = $v->invoice_items;
+                //     $v['invoice_items'] = $inv_items;
 
-                });
+                // });
                 // foreach ($invoices as $key => $value) {
                 //     $invoice_items = $value->invoice_items;
                 //     $invoices[$key]['invoice_items'] = $invoice_items;
