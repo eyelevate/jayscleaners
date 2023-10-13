fc13ae8278a3177fcb3b1592bb5a95669853cb72
wondochoung <onedough83@gmail.com>
2023-10-12 21:07:15 -0500

    test

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -2,22 +2,33 @@
 
 namespace App\Http\Middleware;
 use Closure;
+use http\Env\Response;
 
 class Cors
 {
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin' , '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application','ip');
-        return $response;
-////         Handle preflight requests
-//        if ($request->isMethod('OPTIONS')) {
-//            return response('', 200)
+//        header('Access-Control-Allow-Origin: *');
+//
+//        // ALLOW OPTIONS METHOD
+//        $headers = [
+//            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+//            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+//        ];
+//        if ($request->getMethod() == "OPTIONS") {
+//            return Response::  response('', 200)
 //                ->header('Access-Control-Allow-Origin', '*')
 //                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 //                ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application','ip');
+//        }
+//
+//        $response = $next($request);
+//        foreach ($headers as $key => $value)
+//            $response->header($key, $value);
+//        return $response;
+////         Handle preflight requests
+//        if ($request->isMethod('OPTIONS')) {
+
 //        }
 //
 //        // Handle actual requests
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,9 +23,11 @@ Route::get('/', ['as'=>'pages_index', 'uses' => 'PagesController@getIndex']);
 | kernel and includes session state, CSRF protection, and more.
 |
 */
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
-
-Route::group(['middleware' => ['cors','web']], function () {
+Route::group(['middleware' => ['web']], function () {
     //public pages
     Route::auth();
 	// Admins
