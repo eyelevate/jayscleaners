a20072bc089d760d227216356cf2097a29d5f8b2
wondochoung <onedough83@gmail.com>
2023-10-12 20:25:40 -0500

    update cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,9 +7,17 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
+        // Handle preflight requests
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', 'https://jays-pos.web.app')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
+        // Handle actual requests
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Origin', 'https://jays-pos.web.app');
     }
 }
 
